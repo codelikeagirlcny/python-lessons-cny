1ba5b8a68bfa73cb8c7732c4aa6de6c48d0d7c78
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-08-22 15:03:45 -0400

    Create ajm.dicts-phonebook.py (in-class exercise)

---
+++ code-exercises-etc/section_10_(dictionaries)/ajm.dicts-phonebook.py
@@ -0,0 +1,23 @@
+phonebook_as_list = [
+    ['Alison','315-481-2904'],
+    ['Shannon','202-555-1234'],
+    ['Brigid','315-555-9876']
+]
+
+#loop, probably with enumerate...
+#conditional asking something like
+# if phonebook[index][0] == 'Shannon' tell me what
+# phonebook[index][1] is.
+
+phonebook_as_dictionary = {
+    'Alison':'315-481-2904',
+    'Shannon':'202-555-1234',
+    'Brigid':'315-555-9876'
+}
+#print phonebook_as_dictionary['Shannon']
+#print phonebook_as_list[1][1]
+
+phonebook_as_dictionary['Mel'] = '301-555-4569'
+print phonebook_as_dictionary
+
+print phonebook_as_dictionary.get('Frankenstein', 'Cannot find anything with that name!')
