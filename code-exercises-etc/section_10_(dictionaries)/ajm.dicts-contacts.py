3401637c45118424f22111d154774eb4c51eb8b5
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-08-22 15:02:59 -0400

    Create ajm.dicts-contacts.py (in-class exercise)

---
+++ code-exercises-etc/section_10_(dictionaries)/ajm.dicts-contacts.py
@@ -0,0 +1,38 @@
+contacts = {
+    'Alison': {
+        'phone':'315-481-2904',
+        'twitter':'@alisonjo2786',
+        'github':'alisonjo2786'
+    },
+    'Shannon': {
+        'phone':'202-555-1234',
+        'twitter':'@svt',
+        'github':'shannonturner'
+    },
+    'Brigid': {
+        'phone' : '315-555-9876',
+        'twitter' : '@therealbrigid',
+        'github' : 'brigidongithub'
+    }
+}
+#print contacts
+
+contacts['Nina'] = {
+    'phone': '301-555-1234',
+    'github': 'ninaGH'
+}
+#print contacts
+
+#list_of_contacts_keys = contacts.keys()
+#for contact in sorted(list_of_contacts_keys):
+
+for contact in sorted(contacts.keys()):
+    print '{0}\'s info:'.format(contact)
+    for contact_type in contacts[contact].keys():
+        print '\t{0}: {1}'.format(contact_type, contacts[contact][contact_type])
+
+#print contacts.items()
+#print contacts.get('Alison').items()
+
+
+
