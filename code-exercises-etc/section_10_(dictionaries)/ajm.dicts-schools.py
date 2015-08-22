614d7ab545196de6c31c513a6b8d4485a2aec00d
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-08-22 15:07:46 -0400

    Create ajm.dicts-schools.py (in-class exercise)

---
+++ code-exercises-etc/section_10_(dictionaries)/ajm.dicts-schools.py
@@ -0,0 +1,20 @@
+schools = {
+    "geometry": {
+        "coordinates": [
+            -81.50572799999999, 
+            39.21675500000001
+        ], 
+        "type": "Point"
+    }, 
+    "properties": {
+        "address": "300 Campus Drive, Parkersburg, WV 26104", 
+        "marker-color": "#3F3040", 
+        "marker-symbol": "circle", 
+        "name": "West Virginia University at Parkersburg"
+    }, 
+    "type": "Feature"
+}
+
+print schools.get('type')
+print schools.get('properties').get('address')
+print schools.get('geometry').get('coordinates')[1]
