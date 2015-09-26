b292f9a1650313b3bc103cd7d0028890c92eff73
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-09-26 14:23:56 -0400

    Create ajm.cond2.20150926.py

---
+++ code-exercises-etc/section_03_(conditionals)/ajm.cond2.20150926.py
@@ -0,0 +1,9 @@
+students = 10
+capacity = 50
+
+if students < capacity:
+    print "Keep recruiting students!"
+elif students == capacity:
+    print "Stop recruiting, we're full."
+else:
+    print "...what happened...?"
