569e08b7b954808172d0b35a73003828d47a70f2
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-09-26 14:16:44 -0400

    Create ajm.str-methods.20150926.py

---
+++ code-exercises-etc/section_02_(strings)/ajm.str-methods.20150926.py
@@ -0,0 +1,18 @@
+email = "AlisonJo@2786@gmail.com"
+#print email.find("@")
+#print email.find("N")
+#print email.find("n")
+#pos = email.find("o")
+#print pos
+
+email = email.lower()
+pos2 = email.find("j")
+print pos2
+
+#print email.count("@")
+
+twitter_name = "@alisonjo2786"
+#print twitter_name
+
+twitter_hashtag = twitter_name.replace("@", "#")
+#print twitter_hashtag
