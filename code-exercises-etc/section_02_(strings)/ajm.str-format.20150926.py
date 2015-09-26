4e7df8e664c3bf118bbdbe9e4dfdde9029e0cb50
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-09-26 14:14:26 -0400

    Create ajm.str-format.20150926.py

---
+++ code-exercises-etc/section_02_(strings)/ajm.str-format.20150926.py
@@ -0,0 +1,18 @@
+name = "Alison"
+age = 29
+#print "My name is {1} and my age is {0}.".format(name, age)
+
+
+phone = "315-481-2904"
+#print "Call {0} for great pizza!".format(phone[4:])
+
+print """Area Code: {0}
+Local: {1}
+Different format: ({0}) {3}-{4}""".format(phone[:3], phone[4:], phone[:3], phone[4:7], phone[8:])
+
+
+tweet = "Saying things that you really want to know"
+length_of_tweet = len(tweet)
+
+#print """That tweet is {0} characters.
+#You have {1} characters left.""".format(len(tweet), 140-len(tweet))
