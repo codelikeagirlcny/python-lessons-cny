914a2813045c9682967f5cd7c7197e2807e2b73e
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-09-26 14:13:36 -0400

    Create ajm.hello.20150926.py

---
+++ code-exercises-etc/section_02_(strings)/ajm.hello.20150926.py
@@ -0,0 +1,25 @@
+#print "Hello world."
+# this is a comment
+#print "this is Alison"
+
+#Create var with twitter handle
+twitter = "@alisonjo2786"
+
+#Output twitter handle to screen
+#print twitter
+#print len(twitter)
+
+print twitter[0]
+print twitter[1:5]
+
+##list_of_lessons = """Lesson\t\tTopic
+##1\t\tStrings and Conditionals
+##2\t\tLists and Loops
+##3\t\tDictionaries & Files"""
+##print list_of_lessons
+##
+##list_of_lessons = "hello"
+##print list_of_lessons
+##
+##list_of_lessons = 5
+##print list_of_lessons
