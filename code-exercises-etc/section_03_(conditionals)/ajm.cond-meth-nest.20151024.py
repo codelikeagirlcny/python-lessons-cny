3b487e226d53e3b024f4fe25bd3d97eacaa96747
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-10-24 14:14:45 -0400

    Create ajm.cond-meth-nest.20151024.py

---
+++ code-exercises-etc/section_03_(conditionals)/ajm.cond-meth-nest.20151024.py
@@ -0,0 +1,11 @@
+gender = "F"
+
+if gender.lower() == 'f':
+    print "Welcome to Code Like a Girl!"
+
+if gender == 'f' or gender == 'F':
+    print "Welcome to Code Like a Girl!"
+    if age >= 21:
+        print "Next round of beer is on you."
+    else:
+        print "Next round of root beer is on you."
