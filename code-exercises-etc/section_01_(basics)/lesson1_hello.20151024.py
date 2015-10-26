560fc9dceaf5bcdec6a5df0cbf6f0460fc388bae
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-10-26 12:27:18 -0400

    Create lesson1_hello.20151024.py

---
+++ code-exercises-etc/section_01_(basics)/lesson1_hello.20151024.py
@@ -0,0 +1,18 @@
+twitter = "@alisonjo2786"
+
+print twitter
+print "twitter"
+
+phone = '315-481-2904'
+
+print phone
+
+#print 'Alison\'s contact info:\nAlison\talisonjo2786@gmail.com\nSarah\tsarah@gmail.com'
+
+print "\n"
+
+lots_of_text = "Lesson\tTopic\n1\tStrings and Conditionals\n2\tLists and Loops\n3\tDictionaries and Files"
+print lots_of_text
+
+length_of_twitter_username = len(twitter)
+print length_of_twitter_username
