29b5117130c4f9f84ba2e34645f5f536b13c5b74
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-10-26 12:28:09 -0400

    Create lesson1_str-format.20151024.py

---
+++ code-exercises-etc/section_02_(strings)/lesson1_str-format.20151024.py
@@ -0,0 +1,17 @@
+name = "Alison"
+age = 29
+phone = "315-481-2904"
+
+##print "My name is"
+##print name
+##print "and my age is"
+##print age
+##print '\n'
+
+print "My name is {0} and my age is {1}.".format(name, age)
+
+print '\n'
+
+message = 'Call {0} for pizza.'.format(phone[4:])
+
+print message
