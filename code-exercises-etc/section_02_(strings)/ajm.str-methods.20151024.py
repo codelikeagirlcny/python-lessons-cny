2bba9fc6f26843347b340a047e5f764f838ce75e
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-10-26 12:40:38 -0400

    Create ajm.str-methods.20151024.py

---
+++ code-exercises-etc/section_02_(strings)/ajm.str-methods.20151024.py
@@ -0,0 +1,12 @@
+email = 'alisonjo2786@gmail.com'
+
+print email.find("@")
+print email.find("Z")
+
+#the_at_sign = email.find("@")
+
+print "\n"
+
+twitter = "@alisonjo2786"
+twitter = twitter.replace("@", "#")
+print twitter
