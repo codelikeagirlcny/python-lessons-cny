d99889ce7010d803dbdebc855bfc2b96b3b8f3f6
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-10-26 12:42:10 -0400

    Create ajm.str-slicing1.20151024.py

---
+++ code-exercises-etc/section_02_(strings)/ajm.str-slicing1.20151024.py
@@ -0,0 +1,13 @@
+twitter = "@alisonjo2786"
+fname = "Alison"
+lname = "McCauley"
+
+#print twitter[-2:-5]
+
+#print fname[0]
+#print lname[0]
+
+phone = "(315) 481-2904"
+
+print phone[1:4]
+print phone[6:9]
