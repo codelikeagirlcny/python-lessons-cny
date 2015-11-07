d60f6da20cbc075a1711f5b676e8597c35d7239f
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 16:51:48 -0500

    Create ajm.lists3-pop-days.20151107.py

---
+++ code-exercises-etc/section_04_(lists)/ajm.lists3-pop-days.20151107.py
@@ -0,0 +1,29 @@
+days_of_week = ['Monday', 'Tuesday']
+
+days_of_week.append('Wednesday')
+#print days_of_week
+#print len(days_of_week)
+days_of_week.append('Thursday')
+##print days_of_week
+##print len(days_of_week)
+days_of_week.append('Friday')
+##print days_of_week
+##print len(days_of_week)
+days_of_week.append('Saturday')
+days_of_week.append('Sunday')
+
+print days_of_week
+print len(days_of_week)
+
+##days_of_week.pop()
+##print days_of_week
+##days_of_week.append('Sunday')
+##print days_of_week
+
+day = days_of_week.pop()
+print days_of_week
+print day
+
+day_again = days_of_week.pop(3)
+print day_again
+print days_of_week
