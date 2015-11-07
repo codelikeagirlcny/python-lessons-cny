8d977eaddffe3a9ff99bfdbb25535d5b9033a33e
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 17:19:17 -0500

    Create ajm.loops5-zip-states.20151107.py

---
+++ code-exercises-etc/section_05_(loops)/ajm.loops5-zip-states.20151107.py
@@ -0,0 +1,9 @@
+state_names = ['Alabama', 'Alaska', 'Arkansas']
+state_abbrs = ['AL', 'AK', 'AR']
+
+for name, abbr in zip(state_names, state_abbrs):
+    print '{0} ({1})'.format(name, abbr)
+
+##state_names.append('Arizona')
+##for name, abbr in zip(state_names, state_abbrs):
+##    print '{0} ({1})'.format(name, abbr)
