73d450b1539170be10a0ff0999f903b741de5f9c
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 16:52:50 -0500

    Create ajm.lists4-extend-months.20151107.py

---
+++ code-exercises-etc/section_04_(lists)/ajm.lists4-extend-months.20151107.py
@@ -0,0 +1,8 @@
+months = ['January', 'February']
+
+months.append('March')
+
+months.extend(['April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+
+print months
+print len(months)
