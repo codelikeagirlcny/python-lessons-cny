66b7656c8f4c2cc2fcb99fdaf34617e3e758dffb
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 16:24:39 -0500

    Create ajm.split-address.20151107.py

---
+++ code-exercises-etc/section_06_(str-list)/ajm.split-address.20151107.py
@@ -0,0 +1,7 @@
+address = '654 E Street SE Washington DC 20003'
+address_as_list = address.split(' ')
+#split_on_this = ' '
+#address_as_list = address.split(split_on_this)
+
+print address
+print address_as_list
