1c3d144912e06ddc987db944370ee5071e0d65f3
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 17:18:46 -0500

    Create ajm.loops4-enumerate-days.20151107.py

---
+++ code-exercises-etc/section_05_(loops)/ajm.loops4-enumerate-days.20151107.py
@@ -0,0 +1,10 @@
+days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+for index, day in enumerate(days_of_week):
+    print 'Day {0}: {1}'.format(index, day)
+
+##for index, day in enumerate(days_of_week):
+##    print 'Day {0}: {1}'.format(index+1, day)
+
+##for leaf, day in enumerate(days_of_week):
+##    print 'Day {0}: {1}'.format(leaf+1, day)
