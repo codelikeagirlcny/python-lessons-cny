19a5db80b727b77a60a6a6c1e227459cd61dbe13
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 16:50:48 -0500

    Create ajm.lists2-append-ages.20151107.py

---
+++ code-exercises-etc/section_04_(lists)/ajm.lists2-append-ages.20151107.py
@@ -0,0 +1,15 @@
+attendees_ages = []
+
+attendees_ages.append(28)
+
+print attendees_ages
+
+attendees_ages.append(30)
+
+print attendees_ages
+
+#attendees_ages.append(18, 35)
+#print attendees_ages
+
+attendees_ages[0] = 25
+print attendees_ages
