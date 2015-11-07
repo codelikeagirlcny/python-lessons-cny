a44327f10e50dc4846903a2fedf8286016828ac5
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 17:13:49 -0500

    Create ajm.loops1-range.20151107.py

---
+++ code-exercises-etc/section_05_(loops)/ajm.loops1-range.20151107.py
@@ -0,0 +1,15 @@
+for number in range(10, 5):
+    print number
+
+    # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    # number = 0
+    # print number
+
+    # number = 1
+    # print number
+
+    # number = 2
+    # print number
+
+#print "{0} (This is after the for loop)".format(number)
