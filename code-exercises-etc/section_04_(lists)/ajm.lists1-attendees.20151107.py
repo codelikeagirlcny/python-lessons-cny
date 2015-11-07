37b985a890dd1fbfd5bd8c9f1e7a4acc5a2d5781
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 16:49:24 -0500

    Create ajm.lists1-attendees.20151107.py

---
+++ code-exercises-etc/section_04_(lists)/ajm.lists1-attendees.20151107.py
@@ -0,0 +1,20 @@
+name = 'Alison'
+attendees = ['Alison', 'Carolyn', 'Mao', 'Liz']
+
+print attendees
+print name
+
+print attendees[0]
+print name[0]
+
+print attendees[3]
+#print attendees[4]
+
+print attendees[1:3]
+
+print "\n\n"
+
+#print len(name)
+#print len(attendees)
+number_of_attendees = len(attendees)
+print number_of_attendees
