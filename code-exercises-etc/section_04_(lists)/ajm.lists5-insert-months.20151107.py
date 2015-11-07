e782ebfa1b49900dcdc1ebad0312cc6e56184188
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 16:53:54 -0500

    Create ajm.lists5-insert-months.20151107.py

---
+++ code-exercises-etc/section_04_(lists)/ajm.lists5-insert-months.20151107.py
@@ -0,0 +1,16 @@
+months = ['January', 'February', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+#print months
+#print len(months)
+
+months.pop(0)
+print months
+months.insert(0, 'January')
+print months
+
+##first_month = months.pop(0)
+##print months
+##print first_month
+##
+##months.insert(0, first_month)
+##print months
