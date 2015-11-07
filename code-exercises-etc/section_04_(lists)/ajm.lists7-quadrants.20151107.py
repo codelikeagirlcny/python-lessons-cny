c46d9a9339777f7517e0b83f86d609e841e473ad
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-11-07 17:02:22 -0500

    Update ajm.lists7-quadrants.20151107.py
    Finished program with remaining quadrants, etc.

--- code-exercises-etc/section_04_(lists)/ajm.lists7-quadrants.20151107.py
+++ code-exercises-etc/section_04_(lists)/ajm.lists7-quadrants.20151107.py
@@ -21,7 +21,7 @@ address2 = raw_input('Enter the second address. ')
 address2 = address2.lower()
 address2_as_list = address2.split(' ')
 if 'nw' in address2_as_list:
-    nw_quadrant.append(address1)
+    nw_quadrant.append(address2)
 elif 'ne' in address2_as_list:
     ne_quadrant.append(address2)
 elif 'se' in address2_as_list:
@@ -31,15 +31,29 @@ elif 'sw' in address2_as_list:
 else:
     'Could not place address2 into a quadrant list.'
 
+address3 = raw_input('Enter the third address. ')
+address3 = address3.lower()
+address3_as_list = address3.split(' ')
+if 'nw' in address3_as_list:
+    nw_quadrant.append(address3)
+elif 'ne' in address3_as_list:
+    ne_quadrant.append(address3)
+elif 'se' in address3_as_list:
+    se_quadrant.append(address3)
+elif 'sw' in address3_as_list:
+    sw_quadrant.append(address3)
+else:
+    'Could not place address3 into a quadrant list.'
+
 ###########
-print "Number of addresses in NW quadrant: {0}".format(len(nw_quadrant))
-print nw_quadrant
+print "\nNumber of addresses in NW quadrant: {0}".format(len(nw_quadrant))
+print "Addresses in NW quadrant: {0}".format(nw_quadrant)
 
-print len(ne_quadrant)
-print ne_quadrant
+print "\nNumber of addresses in NE quadrant: {0}".format(len(ne_quadrant))
+print "Addresses in NE quadrant: {0}".format(ne_quadrant)
 
-print len(se_quadrant)
-print se_quadrant
+print "\nNumber of addresses in SE quadrant: {0}".format(len(se_quadrant))
+print "Addresses in SE quadrant: {0}".format(se_quadrant)
 
-print len(sw_quadrant)
-print sw_quadrant
+print "\nNumber of addresses in SW quadrant: {0}".format(len(sw_quadrant))
+print "Addresses in SW quadrant: {0}".format(sw_quadrant)
