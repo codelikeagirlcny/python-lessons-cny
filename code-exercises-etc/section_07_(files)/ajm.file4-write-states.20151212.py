4c20aab36e65aac6865757e121867410e75b709e
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2016-01-17 16:32:09 -0500

    Create ajm.file4-write-states.20151212.py

---
+++ code-exercises-etc/section_07_(files)/ajm.file4-write-states.20151212.py
@@ -0,0 +1,23 @@
+state_abbrs = ''
+state_names = ''
+
+with open('states.csv', 'r') as states_file:
+    states = states_file.read().split('\n')
+
+for index,state in enumerate(states):
+    states[index] = state.split(',')
+
+    state_abbrs += states[index][0] + '\n'
+    state_names += states[index][1] + '\n'
+
+with open('state_abbreviations.20151212.txt', 'w') as abbr_file:
+    abbr_file.write(state_abbrs)
+
+with open('state_names.20151212.txt', 'w') as name_file:
+    name_file.write(state_names)
+
+#if we do this with lists of abbrs and names instead of
+#strings (like in the previous exercise), we can convert
+#the lists into strings:
+#   state_abbrs_as_string = '\n'.join(state_abbrs)
+#(and then write to the file)
