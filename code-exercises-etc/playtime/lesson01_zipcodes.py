683533b91f2448eddbf1fef17350c96d01f79649
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2016-01-31 12:49:24 -0500

    Update lesson01_zipcodes.py
    added study buddy addresses

--- code-exercises-etc/playtime/lesson01_zipcodes.py
+++ code-exercises-etc/playtime/lesson01_zipcodes.py
@@ -2,6 +2,12 @@
 
 # NOTE: Please don't use anyone's *real* contact information during these exercises, especially because you're putting them up on GitHub!
 
-studybuddy_1 = 'Alison McCauley, 1234 Maple Street, Skaneateles, NY 13152'
+# For the purposes of this exercise, we're going to use unrealistically perfect and uniform addresses :)
 
-# Goal 1: 
+studybuddy_1 = 'Alison McCauley, 1234 County Line Road, Skaneateles, NY 13152'
+studybuddy_2 = 'Dee Cater, 555 Bradford Pkwy, Syracuse, NY 13224'
+studybuddy_3 = 'Andrea Bianchi, 987 South St, Jamesville, NY 13078'
+studybuddy_4 = 'Kristen Link Logan, 264 Craig Street, Syracuse, NY 13211'
+studybuddy_5 = 'Nina Verity, 111 Bridge St, Solvey, NY 13209'
+
+# Goal 1: Output the ZIP code of each study buddy.
