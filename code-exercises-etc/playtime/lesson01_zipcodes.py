dcb27a7e7e31cb814e53f91ff609d81b488e376e
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2016-01-31 12:24:47 -0500

    Create lesson01_zipcodes.py

---
+++ code-exercises-etc/playtime/lesson01_zipcodes.py
@@ -0,0 +1,7 @@
+# Challenge Level: Intermediate and Advanced
+
+# NOTE: Please don't use anyone's *real* contact information during these exercises, especially because you're putting them up on GitHub!
+
+studybuddy_1 = 'Alison McCauley, 1234 Maple Street, Skaneateles, NY 13152'
+
+# Goal 1: 
