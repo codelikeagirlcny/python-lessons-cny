ce94a34f53712068c86abed118d2f1ee9a47aa5a
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2016-03-19 12:46:12 -0400

    added raw_input example in-class exercise

---
+++ code-exercises-etc/ajm.raw_input.20160319.py
@@ -0,0 +1,12 @@
+print "hello there, {0}!".format(raw_input('Enter your name.'))
+
+attendees = []
+
+attendee1_name = raw_input('Provide the first attendee name. ')
+
+print attendee1_name
+
+attendees.append(attendee1_name)
+print attendees
+print len(attendees)
+
