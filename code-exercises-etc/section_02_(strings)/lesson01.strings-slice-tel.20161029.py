c065b218156e2ce8f58be4c7601122f7d44785be
Alison McCauley <alisonjo2786@users.noreply.github.com>
2016-11-05 16:53:58 -0400

    Add files via upload

---
+++ code-exercises-etc/section_02_(strings)/lesson01.strings-slice-tel.20161029.py
@@ -0,0 +1,39 @@
+phone = "(315) 481-2904"
+##
+####print phone[1:4]
+####print phone[6:9]
+##
+##area_code = phone[1:4]
+##print area_code
+
+#phone_length = len(phone)
+#phone_4 = phone.find("4")
+
+pizza_place = "Alison's Pizza"
+phone = "315-481-2904"
+#print "Call {0} for great pizza.".format(phone)
+#print "Call {1}, which is the number for {0}, for great pizza.".format(pizza_place, phone[4:])
+
+tweet = """In just over one year, @hearmecode has reached over 800 members who are learning how to code together."""
+
+##print """The tweet is {0} characters.
+##You have {1} characters left.""".format(len(tweet), 140-len(tweet))
+
+##tweet_maxlength = 140
+##
+##print """The tweet is {0} characters.
+##You have {1} characters left.""".format(len(tweet), tweet_maxlength - len(tweet))
+
+phone = "315-481-2904"
+
+#print "-Different Format: ({0}) {1}".format(phone[:3], phone[4:])
+
+
+
+
+
+
+
+
+
+
