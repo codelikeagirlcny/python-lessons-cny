634b1ac98a0f04c16b039015cf36743afd22c5c4
Alison McCauley <alisonjo2786@users.noreply.github.com>
2016-11-05 16:50:10 -0400

    added lesson01.basics-hello-world.20161029.py via upload

---
+++ code-exercises-etc/section_01_(basics)/lesson01.basics-hello-world.20161029.py
@@ -0,0 +1,21 @@
+# here we're doing our first print!
+##print "hello world"
+##
+##tweet = '''We're here at "Code Like A Girl"
+##blah blah blah blaha nnd other stuff too...'''
+
+#print tweet
+
+#print "Lesson      Topic\n 1    Strings and conditionals\n 2      Lists and Loops\n3      Dictionaries & Files"
+
+#print "Lesson\tTopic \n1 \tStrings and Conditionals"
+
+#print '''Lesson\tTopic
+#\tStrings and Conditionals'''
+
+##lesson_topics = "Lesson\tTopic \n1 \tStrings and Conditionals"
+##print lesson_topics
+
+twitter = "@alisonjo2786"
+len(twitter)
+print len(twitter)
