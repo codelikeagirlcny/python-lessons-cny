756698508ca8e91fb5cebefcd967b045ee134315
Alison McCauley <alisonjo2786@users.noreply.github.com>
2017-01-29 15:31:12 -0500

    Add files via upload

---
+++ code-exercises-etc/section_01_(basics)/lesson01.basics-hello-world.20170129.py
@@ -0,0 +1,24 @@
+#print "Welcome to Python Lesson 1"
+
+twitter = "@alisonjo2786"
+#print twitter
+
+#print "twitter"
+
+phone1 = "315-481-2904"
+phone2 = 315-481-2904
+
+#print phone1
+#print phone2
+
+print "\tTopic\n1\tStrings and Conditionals\n2\tLists and Loops\n3\tDictionaries and Files"
+
+print """\nLesson\tTopic
+1  Strings and Conditionals
+2  Lists and Loops"""
+
+
+print len(twitter)
+print len("hello world")
+
+
