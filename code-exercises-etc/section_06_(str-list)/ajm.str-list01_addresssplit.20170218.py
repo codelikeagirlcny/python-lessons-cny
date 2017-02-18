c639ac19ca6cbfb32d50f521a21872dcf243f7ec
Alison <alison@cornell.edu>
2017-02-18 16:40:20 -0500

    added 20170218 in-class exercise file for str-list

---
+++ code-exercises-etc/section_06_(str-list)/ajm.str-list01_addresssplit.20170218.py
@@ -0,0 +1,8 @@
+address = "1621 West Lake Road, Skaneateles, NY, 13152"
+print address
+
+address_as_list_1 = address.split(" ")
+print address_as_list_1
+
+address_as_list_2 = address.split(",")
+print address_as_list_2
