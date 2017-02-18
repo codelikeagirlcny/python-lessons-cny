86032cb9c9a3f3314d4ab8776a24c33f779db8ba
Alison McCauley <alisonjo2786@users.noreply.github.com>
2017-02-18 16:09:14 -0500

    Delete lesson02.lists04_addresssplit.20170218.py

--- code-exercises-etc/section_04_(lists)/lesson02.lists04_addresssplit.20170218.py
+++
@@ -1,8 +0,0 @@
-address = "1621 West Lake Road, Skaneateles, NY, 13152"
-print address
-
-address_as_list_1 = address.split(" ")
-print address_as_list_1
-
-address_as_list_2 = address.split(",")
-print address_as_list_2
