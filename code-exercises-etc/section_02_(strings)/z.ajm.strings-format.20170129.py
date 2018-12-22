1e4d46cc2aa899daa28ff4444e063f6e87e0af7b
Alison Jo McCauley <alisonjo2786@users.noreply.github.com>
2018-12-22 18:01:21 -0500

    renamed ajm in-class notes to start with z. so that "official" materials are at the top

--- code-exercises-etc/section_02_(strings)/z.ajm.strings-format.20170129.py
+++ code-exercises-etc/section_02_(strings)/z.ajm.strings-format.20170129.py
@@ -3,8 +3,8 @@ age = 30
 
 print "My name is {0} and my age is {1}".format(name, age)
 
-phone = "315-481-2904"
+phone = "315-555-2955"
 print "Call {0} for great pizza".format(phone[4:])
 
-phone = "315 481 2904"
+phone = "315 555 2955"
 print "Different format: ({0})-{1}-{2}".format(phone[0:3], phone[4:7], phone[8:])
