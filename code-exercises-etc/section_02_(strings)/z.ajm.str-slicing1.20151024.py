06ee2452df41ed45d761233ed1c1bc4cebf5625a
Alison Jo McCauley <alisonjo2786@users.noreply.github.com>
2018-12-22 18:00:24 -0500

    renamed ajm in-class notes to start with z. so that "official" materials are at the top

--- code-exercises-etc/section_02_(strings)/z.ajm.str-slicing1.20151024.py
+++ code-exercises-etc/section_02_(strings)/z.ajm.str-slicing1.20151024.py
@@ -1,13 +1,13 @@
 twitter = "@alisonjo2786"
 fname = "Alison"
-lname = "McCauley"
+lname = "Oppedibango"
 
 #print twitter[-2:-5]
 
 #print fname[0]
 #print lname[0]
 
-phone = "(315) 481-2904"
+phone = "(315) 555-2955"
 
 print phone[1:4]
 print phone[6:9]
